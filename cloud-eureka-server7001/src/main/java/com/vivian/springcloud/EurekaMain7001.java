32f32ecf1fcac39e667ba23f4607a7991c187f89
Questzzq <1073371323@qq.com>
2021-09-15 23:53:58 +0800

    添加Eureka单机模式

--- cloud-eureka-server7001/src/main/java/com/vivian/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/vivian/springcloud/EurekaMain7001.java
@@ -2,16 +2,16 @@ package com.vivian.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author EricTseng
- * @date 2021/9/14 21:04
+ * @date 2021/9/15 22:59
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain8002 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain8002.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
